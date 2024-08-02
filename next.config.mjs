4ea2c691b1b5d6d0c9f634889229be77cf5d2430
ericselva <eric.selva@impackta.com>
2024-08-02 17:02:38 +0200

    update dependencies

--- next.config.mjs
+++ next.config.mjs
@@ -2,21 +2,29 @@
 import TerserPlugin from "terser-webpack-plugin";
 
 const nextConfig = {
-  webpack: (config, { dev /*isServer*/ }) => {
-    if (dev) {
-      config.optimization.minimize = false;
-    } else {
-      config.optimization.minimizer = [
-        new TerserPlugin({
-          terserOptions: {
-            compress: true,
-          },
-        }),
-      ];
-    }
+	webpack: (config, { dev /*isServer*/ }) => {
+		if (dev) {
+			config.optimization.minimize = false;
+		} else {
+			config.optimization.minimizer = [
+				new TerserPlugin({
+					terserOptions: {
+						compress: true,
+					},
+				}),
+			];
+		}
 
-    return config;
-  },
+		return config;
+	},
+	images: {
+		// domains: ['test.com'],
+		deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+		imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+	},
+	compiler: {
+		styledComponents: true,
+	},
 };
 
 export default nextConfig;
