9158a4cae706ece5911bcba96f6f0e9bffbba615
ericselva <eric.selva@impackta.com>
2024-08-03 20:33:34 +0200

    add types

--- src/types/layout.d.ts
+++ src/types/layout.d.ts
@@ -1,6 +1,6 @@
 import type { ReactNode } from "react";
 
-export interface LayoutProps {
+interface LayoutProps {
 	children: ReactNode;
 	hide?: boolean;
 }
--- src/types/movies.d.ts
+++ src/types/movies.d.ts
@@ -1,13 +1,33 @@
-export interface Movie {
+interface Movie {
 	id: number;
 	title: string;
 	poster_path: string;
 	favorite?: boolean;
 }
 
-export interface Movies {
+interface Movies {
 	page: number;
 	results: Movie[];
 	total_pages: number;
 	total_results: number;
 }
+interface MovieDetailProps {
+	initialMovie: Movie | null;
+}
+interface MovieDetail {
+	id: number;
+	title: string;
+	favorite?: boolean;
+	production_companies?: {
+		id: number;
+		logo_path: string;
+		name: string;
+	}[];
+	backdrop_path?: string;
+	overview?: string;
+	release_date?: string;
+	vote_average?: number;
+	vote_count?: number;
+}
+
+export type { Movie, Movies, MovieDetailProps, MovieDetail };
