11972594e5346367f763d32c439a57709cf2eaf1
ericselva <eric.selva@impackta.com>
2024-08-03 12:33:58 +0200

    add initial fetch at index

--- src/hooks/useHome.tsx
+++ src/hooks/useHome.tsx
@@ -1,35 +1,12 @@
-import { useAppContext } from "@/context/AppContext";
-import { apiPaths } from "@/data/api";
-import fetchData from "@/utils/api";
 import { useEffect } from "react";
-
+import useMovies from "./useMovies";
 const useHome = () => {
-	const { setLoading, setResults } = useAppContext();
-	const { discover, movie } = apiPaths;
-
+	const { getMovies } = useMovies();
+	// biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
 	useEffect(() => {
 		getMovies();
 	}, []);
 
-	const getMovies = async () => {
-		setLoading(true);
-
-		try {
-			const data = await fetchData({
-				path: '/movie/popular',
-				// path: `${discover}/${movie}`,
-			});
-			console.log(data);
-
-			// setResults(data.data.results);
-		} catch (error) {
-			setResults("No results found");
-		} finally {
-			setLoading(false);
-		}
-	};
-
-	return { getMovies };
 };
 
 export default useHome;
