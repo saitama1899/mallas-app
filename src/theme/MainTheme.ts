0ac407efa5271e010e588f5f498a53fd056f3f49
ericselva <eric.selva@impackta.com>
2024-08-03 20:33:10 +0200

    add new colors

--- src/theme/MainTheme.ts
+++ src/theme/MainTheme.ts
@@ -4,6 +4,8 @@ export const MainTheme: DefaultTheme = {
 	colors: {
 		primary: "#EC1D24",
 		input_placeholder: "#AAAAAA",
+		border: "#333333",
+		scrollbar: "#D9D9D9",
 	},
 	layout: {
 		headerHeight: "84px",
