6a083b77ddd964aaa441a063fc017215ba6842c2
ericselva <eric.selva@impackta.com>
2024-08-03 20:34:26 +0200

    fix layout height

--- src/components/layout/Layout/Layout.style.tsx
+++ src/components/layout/Layout/Layout.style.tsx
@@ -9,6 +9,7 @@ const LayoutWrapper = styled.div`
 const MainContent = styled.main<{ hide: boolean }>`  
   flex: 1;
   margin-top: ${({ hide, theme }) => (hide ? "0" : theme.layout.headerHeight)};
+  height: calc(100vh - ${({ theme }) => theme.layout.headerHeight});
 `;
 
 export { LayoutWrapper, MainContent };
