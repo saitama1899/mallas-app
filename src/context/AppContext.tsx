54a0dcdc5cc0b88e36bebfca9515c3f559a70dff
ericselva <eric.selva@impackta.com>
2024-08-03 12:34:42 +0200

    add movies context

--- src/context/AppContext.tsx
+++ src/context/AppContext.tsx
@@ -1,30 +1,39 @@
-import type React from 'react';
-import { type ReactNode, createContext, useContext, useState } from 'react'
+import { initialMovies } from "@/data/movies";
+import type { Movies } from "@/types/movies";
+import type React from "react";
+import { type ReactNode, createContext, useContext, useState } from "react";
 
 interface AppContextProps {
-  loading: boolean;
-  results: string;
-  setLoading: React.Dispatch<React.SetStateAction<boolean>>;
-  setResults: React.Dispatch<React.SetStateAction<string>>;
+	loading: boolean;
+	results: string;
+	movies: Movies;
+	setLoading: React.Dispatch<React.SetStateAction<boolean>>;
+	setResults: React.Dispatch<React.SetStateAction<string>>;
+	setMovies: React.Dispatch<React.SetStateAction<Movies>>;
 }
 
 const AppContext = createContext<AppContextProps | undefined>(undefined);
 
-export const AppProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
-  const [loading, setLoading] = useState(false);
-  const [results, setResults] = useState("");
+export const AppProvider: React.FC<{ children: ReactNode }> = ({
+	children,
+}) => {
+	const [loading, setLoading] = useState(false);
+	const [results, setResults] = useState("");
+	const [movies, setMovies] = useState<Movies>(initialMovies);
 
-  return (
-    <AppContext.Provider value={{ loading, results, setLoading, setResults }}>
-      {children}
-    </AppContext.Provider>
-  );
+	return (
+		<AppContext.Provider
+			value={{ loading, results, movies, setLoading, setResults, setMovies }}
+		>
+			{children}
+		</AppContext.Provider>
+	);
 };
 
 export const useAppContext = () => {
-  const context = useContext(AppContext);
-  if (context === undefined) {
-    throw new Error('useAppContext must be used within an AppProvider');
-  }
-  return context;
+	const context = useContext(AppContext);
+	if (context === undefined) {
+		throw new Error("useAppContext must be used within an AppProvider");
+	}
+	return context;
 };
