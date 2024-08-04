31b564e17226cf2bae1ddb078738940f35878999
ericselva <eric.selva@impackta.com>
2024-08-04 07:31:06 +0200

    add bundle analyzer

--- next.config.mjs
+++ next.config.mjs
@@ -1,24 +1,40 @@
-/** @type {import('next').NextConfig} */
-import TerserPlugin from "terser-webpack-plugin";
+import { BundleAnalyzerPlugin } from "webpack-bundle-analyzer";
 
 const nextConfig = {
-	webpack: (config, { dev /*isServer*/ }) => {
+	webpack: (config, { dev, isServer }) => {
+		if (!dev && !isServer) {
+			config.plugins.push(
+				new BundleAnalyzerPlugin({
+					analyzerMode: "static",
+					reportFilename: "../bundles/report.html",
+					openAnalyzer: false,
+				}),
+			);
+		}
+
 		if (dev) {
 			config.optimization.minimize = false;
 		} else {
+			config.optimization.minimize = true;
 			config.optimization.minimizer = [
 				new TerserPlugin({
 					terserOptions: {
 						compress: true,
+						mangle: true,
+						output: {
+							comments: false,
+						},
 					},
 				}),
 			];
 		}
 
+		config.optimization.concatenateModules = true;
+
 		return config;
 	},
 	images: {
-		// domains: ['test.com'],
+		domains: ["https://mallas-app.vercel.app"],
 		deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
 		imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
 	},
